590215e9fcd9bf583aad252147a7ea59e0183a62
Arthur Medeiros <rthurmedeiros@gmail.com>
2019-05-13 11:23:55 -0300

    Criação dinâmica de threads

--- pthreads.c
+++ pthreads.c
@@ -2,12 +2,18 @@
 #include <stdlib.h>
 #include <pthread.h>
 
+void *primo(void *args) {
+	// Thread 0: O número 1 é primo
+}
+
 int main () {
 	int n_valores = 0, n_threads = 0;
+	pthread_t* threads = (pthread_t *) malloc(sizeof(pthread_t)) ;
 	printf("Digite a quantidade de números a processar: ");
 	scanf("%d", &n_valores);
 	printf("Digite a quantidade de threads a criar: ");
 	scanf("%d", &n_threads);
+	threads = (pthread_t *) realloc(threads, n_threads * sizeof(pthread_t));
 	// O número total de números primos encontrados foi: 5
 	return 1;
 }
