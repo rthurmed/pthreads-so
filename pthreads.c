ee1235e3c41928610c89c3c8df270e7f5f155ade
Arthur Medeiros <rthurmedeiros@gmail.com>
2019-05-13 11:18:46 -0300

    Leitura dos valores iniciais

--- pthreads.c
+++ pthreads.c
@@ -4,6 +4,10 @@
 
 int main () {
 	int n_valores = 0, n_threads = 0;
-	
+	printf("Digite a quantidade de números a processar: ");
+	scanf("%d", &n_valores);
+	printf("Digite a quantidade de threads a criar: ");
+	scanf("%d", &n_threads);
+	// O número total de números primos encontrados foi: 5
 	return 1;
 }
